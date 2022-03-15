2d74a4409d1a7b3988994fe2441dce4dd992234a
Andrew Kapp <dkapp59@gmail.com>
2022-03-15 10:36:17 -0500

    updated programs router to get all the details of each program and thier organizations

--- src/components/OrganizationsList/OrganizationItem.jsx
+++ src/components/OrganizationsList/OrganizationItem.jsx
@@ -13,7 +13,7 @@ function OrganizationItem({ organization }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const handleSelectedOrganization = (movie) => {
+  const handleSelectedOrganization = (organization) => {
     dispatch({ type: 'SET_SELECTED_ORGANIZATION', payload: organization });
     history.push('/organization-details');
   };
@@ -30,11 +30,16 @@ function OrganizationItem({ organization }) {
         component="img"
         height="100"
         image={img}
-        onClick={() => handleSelectedOrganization(organization)}
-        alt={(name, 'Poster')}
+        // onClick={() => handleSelectedOrganization(organization)}
+        alt={(name, 'img')}
       />
       <CardContent sx={{ maxHeight: 60 }}>
-        <Typography gutterBottom variant="h6" component="div">
+        <Typography 
+        gutterBottom 
+        variant="h6" 
+        component="div"
+        onClick={() => handleSelectedOrganization(organization)}
+        >
           {name}
         </Typography>
       </CardContent>
